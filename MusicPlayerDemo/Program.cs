6da4131ed8a7c4bedb7f70befc0d497e0d5b454e
Luis Medrano <you@example.com>
2025-04-11 23:05:08 -0500

    Usando la clase Artist y Song

--- MusicPlayerDemo/Program.cs
+++ MusicPlayerDemo/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Song song1 = new Song("Thriller","Billie Jean","Una canción icónica con un bajo inolvidable que habla sobre una mujer que afirma que el hijo del cantante es suyo.","Pop");
+            song1.Album = "Thriller";
+            Console.WriteLine(song1.Album);
+
+            Song song2 = new Song("Play","Cuando Pienses en Volver","Una balada nostálgica que expresa el deseo de recuperar un amor que se fue.","Rock/Pop");
+            song2.Album = "Play";
+            Console.WriteLine(song2.Album);
+
+            Artist artist1 = new Artist("Michael Jackson","Pop","Thriller","Epic Records",13);
+            artist1.album = "Thriller";
+            Console.WriteLine(artist1.album);
+
+            Artist artist2 = new Artist("Pedro Suárez Vértiz","Rock/Pop","Play","Sony Music",0);
+            artist2.album = "Play";
+            Console.WriteLine(artist2.album);
         }
     }
 }
